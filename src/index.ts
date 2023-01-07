070c246f779b0713fdd50f6946e001aa432389f5
Artjom Kurapov <artkurapov@gmail.com>
2023-01-07 22:52:59 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify'
+
+const server = fastify()
+
+server.get('/', async (request, reply) => {
+  return 'Hello there! 👋'
+})
+
+server.listen({
+    host:'0.0.0.0',
+    port: 1234
+}, (err, address) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Started server at ${address}`)
+})
